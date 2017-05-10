e795fcf2696c83041867c70b6040d52931aa2a37
Brian Martinez <brianmartinez@Brian-Martinezs-iMac.local>
2017-05-10 06:02:16 -0500

    Added charge store and a1 order history twig files and modified index twig file...Also added base twig file to Default folder, but notices that its reading base twig file from app/Resources/views folder

--- src/InventoryBundle/Controller/DefaultController.php
+++ src/InventoryBundle/Controller/DefaultController.php
@@ -14,4 +14,20 @@ class DefaultController extends Controller
     {
         return $this->render('InventoryBundle:Default:index.html.twig');
     }
+
+    /**
+     * @Route("/charge")
+     */
+    public function chargeAction()
+    {
+        return $this->render('InventoryBundle:Default:charge_store.html.twig');
+    }
+
+    /**
+     * @Route("/a1")
+     */
+    public function  orderhistory1Action()
+    {
+        return $this->render('InventoryBundle:Default:a1_order_history.html.twig');
+    }
 }
