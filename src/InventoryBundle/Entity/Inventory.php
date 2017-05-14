47b3c720a19bff4e3f19a98ae64f9b412753ccc9
Samir Patel <samir.patel@flosports.tv>
2017-05-14 08:24:11 -0500

    Added product description field

--- src/InventoryBundle/Entity/Inventory.php
+++ src/InventoryBundle/Entity/Inventory.php
@@ -21,9 +21,24 @@ class Inventory
     protected $id;
 
     /**
+     * @todo brian Typically properties are camelCased and field names are snake_cased
      * @ORM\Column(type="string")
      */
     protected $product_name;
+
+    /**
+     * @todo brian Also include some kind of textual description for the field if applicable
+     *
+     * This is the marketing description for the item to assist with display and search
+     *
+     * @todo brian Note how the name here is the name of the field in the DB, but your code will reference the field as productDescription
+     * Also note how the length property is set and the nullable flag will tell you that this field is optional
+     *
+     * @ORM\Column(name="product_description", type="string", length=255, nullable=true)
+     *
+     * @var string
+     */
+    protected $productDescription;
 
     /**
      * @ORM\Column(type="float")
