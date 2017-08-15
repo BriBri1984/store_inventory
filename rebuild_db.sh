15289ce36b28d02f5c084dcb47eeb46b657e1daf
Samir Patel <samir.patel@flosports.tv>
2017-08-15 15:14:36 -0500

    Rebuild

--- rebuild_db.sh
+++ rebuild_db.sh
@@ -2,4 +2,4 @@
 
 ./bin/console doctrine:database:drop --force
 ./bin/console doctrine:database:create
-./bin/console doctrine:migrations:migrate --no-interaction
+./bin/console doctrine:schema:update --force
